2ada6c3cdc4406be3b15c45335eb57623f15e8e8
hwangstone1 <mylee553@naver.com>
2022-03-03 00:41:01 +0900

    '-'

--- board/views.py
+++ board/views.py
@@ -4,6 +4,7 @@ from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -61,3 +62,60 @@ def answer_create(request, question_id):
         form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'board/question_detail.html', context)
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+
+    answer = get_object_or_404(request, pk= answer_id)
+    if request.user != answer.author:
+        messages.error('수정 권한이 없습니다')
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date=timezone.now()
+            answer.save()
+            return redirect('board/detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer' : answer, 'form' : form}
+    return render(request, 'board/answer_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+
+    """
+    질문 수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('board:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('board:detail', question_id=question.id)
+
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form':form}
+    return render(request, 'board/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제 권한이 없습니다')
+        return redirect('board/detail', question_id=question.id)
+
+    question.delete()
+    return redirect('board:index')
+
+
